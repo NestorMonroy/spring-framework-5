7dce8bc0ee58464bc3f708d5598d6c2b7524fda7
NestorMonroy <nestor.monroy.90@gmail.com>
2022-10-31 10:34:47 -0600

    [add] example scople

--- spring-boot-di/src/main/java/mx/nestor/ideas/di/app/models/domain/Factura.java
+++ spring-boot-di/src/main/java/mx/nestor/ideas/di/app/models/domain/Factura.java
@@ -1,14 +1,25 @@
 package mx.nestor.ideas.di.app.models.domain;
 
+import java.io.Serializable;
 import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.ApplicationScope;
+import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.annotation.SessionScope;
 
 @Component
-public class Factura {
+//@SessionScope
+@RequestScope
+public class Factura implements Serializable{
+
+	private static final long serialVersionUID = 5562524110368621819L;
 
 	@Value("${factura.descripcion}")
 	private String descripcion;
@@ -20,6 +31,19 @@ public class Factura {
 	//@Qualifier("itemsFactura")
 	private List<ItemFactura> items;
 
+	
+	@PostConstruct
+	public void inicializar() {
+		cliente.setNombre(cliente.getNombre().concat(" ").concat("David"));
+		descripcion = descripcion.concat(" del cliente: ").concat(cliente.getNombre());
+	}
+	
+	@PreDestroy
+	public void destruir() {
+		System.out.println("Factura destruida: ".concat(descripcion));
+	}
+
+	
 	public String getDescripcion() {
 		return descripcion;
 	}
