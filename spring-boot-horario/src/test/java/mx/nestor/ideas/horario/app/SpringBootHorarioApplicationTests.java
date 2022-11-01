6432d4d63529e2aeb93ae83abeadd2831db534b3
NestorMonroy <nestor.monroy.90@gmail.com>
2022-11-01 16:42:47 -0600

    [add] example HorarioInterceptor

---
+++ spring-boot-horario/src/test/java/mx/nestor/ideas/horario/app/SpringBootHorarioApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.nestor.ideas.horario.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHorarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
